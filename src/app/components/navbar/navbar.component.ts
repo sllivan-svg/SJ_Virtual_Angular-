7030ca9a8f21ffacc43d8c952404b4b0d5aba29d
kevincollazos <kcollazos09@gmail.com>
2020-11-25 13:42:28 -0500

    Cambios basicos
    Se desarrolla la logica para cerrar sesion

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import Swal from 'sweetalert2';
+import { Location } from '@angular/common';
+
+//services
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-navbar',
@@ -7,9 +12,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  constructor(  private auth: AuthService, private location: Location ) { }
 
   ngOnInit(): void {
+  }
+
+  salir(){
+       
+    this.auth.logout();
+   
+    Swal.fire({
+          icon: 'success',
+          title: 'Se cerro la sesion',
+        });
+
+    this.location.replaceState('/login');
+    location.reload();
   }
 
 }
